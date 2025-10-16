e1d21dbc15fb4e1a85f842f8bfc064e47feb9b97
Daniel Precioso, PhD <danipg1409@gmail.com>
2025-10-16 11:49:32 +0200

    Allow to choose parameters

--- scripts/paper_results_benchmark.py
+++ scripts/paper_results_benchmark.py
@@ -6,7 +6,18 @@ from routetools.benchmark import optimize_benchmark_instance
 from routetools.plot import plot_curve
 
 
-def single_run(instance_name: str):
+def single_run(
+    instance_name: str,
+    penalty: int = 10,
+    K: int = 6,
+    L: int = 64,
+    num_pieces: int = 1,
+    popsize: int = 200,
+    sigma0: int = 1,
+    tolfun: float = 0.0001,
+    damping: float = 1,
+    maxfevals: int = 25000,
+):
     """Run a single benchmark instance and save the result to output/."""
     dict_instance = load(
         instance_name,
@@ -20,14 +31,15 @@ def single_run(instance_name: str):
 
     curve_opt, info = optimize_benchmark_instance(
         dict_instance,
-        K=6,
-        L=128,
-        penalty=10,
-        num_pieces=1,
-        maxfevals=1000000,
-        tolfun=1e-5,
-        popsize=1000,
-        sigma0=3,
+        penalty=penalty,
+        K=K,
+        L=L,
+        num_pieces=num_pieces,
+        popsize=popsize,
+        sigma0=sigma0,
+        tolfun=tolfun,
+        damping=damping,
+        maxfevals=maxfevals,
     )
     vectorfield = info["vectorfield"]
     land = info["land"]
@@ -47,7 +59,17 @@ def single_run(instance_name: str):
     plt.close()
 
 
-def main():
+def main(
+    penalty: int = 10,
+    K: int = 6,
+    L: int = 128,
+    num_pieces: int = 1,
+    popsize: int = 1000,
+    sigma0: int = 3,
+    tolfun: float = 1e-5,
+    damping: float = 1,
+    maxfevals: int = 1000000,
+):
     """Run benchmark instances and save the results to output/."""
     ls_instances = [
         "DEHAM-USNYC",
@@ -59,7 +81,18 @@ def main():
     ]
     for instance in ls_instances:
         print(f"Running benchmark for instance {instance}")
-        single_run(instance)
+        single_run(
+            instance,
+            penalty=penalty,
+            K=K,
+            L=L,
+            num_pieces=num_pieces,
+            popsize=popsize,
+            sigma0=sigma0,
+            tolfun=tolfun,
+            damping=damping,
+            maxfevals=maxfevals,
+        )
 
 
 if __name__ == "__main__":
