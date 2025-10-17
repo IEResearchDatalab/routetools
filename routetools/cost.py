Fix cost calculation by re-enabling gradient stopping for infeasible paths

routetools/cost.py
@@ -126,8 +126,7 @@ def cost_function_constant_speed_time_invariant(
     # Cost is the time to travel the segment
     dt = jnp.sqrt(d2 / (v2 - w2) + dw**2 / (v2 - w2) ** 2) - dw / (v2 - w2)
     # Current > speed -> infeasible path
-    # dt = lax.stop_gradient(jnp.where(v2 <= w2, 1e10, 0.0))
-    # t_total = jnp.sum(dt, axis=1)
+    dt = lax.stop_gradient(jnp.where(v2 <= w2, 1e10, 0.0))
     return dt
 
 